897fe61f84c8e8342d87ed1c192a7ef86d24effc
miguelsantos <miguel@miguelsantos.com.br>
2025-04-02 19:57:19 -0300

    organize tests and add repr test for TextNode

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -4,31 +4,34 @@ from textnode import TextNode, TextType
 
 
 class TestTextNode(unittest.TestCase):
-    def test_eq_bold(self):
-        node = TextNode("This is a text node", TextType.BOLD)
-        node2 = TextNode("This is a text node", TextType.BOLD)
-        self.assertEqual(node, node2)
+    def test_eq(self):
+        bold_node = TextNode("This is a text node", TextType.BOLD)
+        bold_node2 = TextNode("This is a text node", TextType.BOLD)
+        self.assertEqual(bold_node, bold_node2)
 
-    def test_eq_link(self):
-        node = TextNode("This is some anchor text", TextType.LINK, "https://www.boot.dev)")
-        node2 = TextNode("This is some anchor text", TextType.LINK, "https://www.boot.dev)")
-        self.assertEqual(node, node2)
+        link_node = TextNode("This is some anchor text", TextType.LINK, "https://www.boot.dev)")
+        link_node2 = TextNode("This is some anchor text", TextType.LINK, "https://www.boot.dev)")
+        self.assertEqual(link_node, link_node2)
 
-    def test_not_eq_bold(self):
-        node = TextNode("This is a text node", TextType.BOLD)
-        node2 = TextNode("This is another text node", TextType.BOLD)
-        self.assertNotEqual(node, node2)
+    def test_not_eq(self):
+        bold_node = TextNode("This is a text node", TextType.BOLD)
+        bold_node2 = TextNode("This is another text node", TextType.BOLD)
+        self.assertNotEqual(bold_node, bold_node2)
 
-    def test_not_eq_link(self):
-        node = TextNode("This is some anchor text", TextType.LINK, "https://www.boot.dev)")
-        node2 = TextNode("This is some anchor text", TextType.LINK)
-        self.assertNotEqual(node, node2)
+        link_node = TextNode("This is some anchor text", TextType.LINK, "https://www.boot.dev)")
+        link_node2 = TextNode("This is some anchor text", TextType.LINK)
+        self.assertNotEqual(link_node, link_node2)
 
     def test_not_eq_normal_bold(self):
         node = TextNode("This is some text", TextType.NORMAL)
         node2 = TextNode("This is some text", TextType.BOLD)
         self.assertNotEqual(node, node2)
 
+    def test_repr(self):
+        node = TextNode("This is a text node", TextType.NORMAL, "https://www.boot.dev")
+        self.assertEqual(
+            "TextNode(This is a text node, normal, https://www.boot.dev)", repr(node)
+        )
 
 if __name__ == "__main__":
     unittest.main()
